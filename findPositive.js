1cb3ea3be7ef0fd34e0defc2459a9da115bbdc18
anandavallis <sanandavalli7@gmail.com>
2025-06-30 13:20:45 +0530

    Create findPositive.js

---
+++ findPositive.js
@@ -0,0 +1,10 @@
+
+        function findFirstPositive(numbers){
+            return numbers.find(num => num > 0);
+        }
+        //Example input
+        const input = [-5, -3, 0, 9, 2];
+        const result = findFirstPositive(input);
+        console.log(result); //output
+        
+    

ba05a2b1a212c98523eca7a22275f39dd9e228bc
anandavallis <sanandavalli7@gmail.com>
2025-06-30 13:17:29 +0530

    Create arrayMethod.js

---
+++ arrayMethod.js
@@ -0,0 +1,9 @@
+
+        function squareEvenNumbers(numbers){
+            return numbers.filter(n => n % 2 === 0).map(n => n * n);
+        }
+         const inputArray = [1, 2, 3, 4, 5, 6] ;
+         const result = squareEvenNumbers(inputArray);
+         console.log(result);
+         
+  
